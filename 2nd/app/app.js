59c9d8ff2661ed9394e009d5667cef9c5228c170
jhmt <jhashimot00@gmail.com>
2016-04-02 15:48:37 +1300

    Update app name

--- 2nd/app/app.js
+++ 2nd/app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('templateStore', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'templateStore.view1',
+  'templateStore.view2',
+  'templateStore.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
